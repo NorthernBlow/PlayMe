38e6892b28d5e481f21b37cfc4ebb4248e968106
Svyatoslav Rheinhart <39205340+NorthernBlow@users.noreply.github.com>
2022-06-23 00:10:04 +0300

    rebase branches

---
+++ play.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import *
+
+#app = QApplication(sys.argv) #создаем приложение
+#dlgMain = QWidget() #главное окно приложения
+#dlgMain.setWindowTitle('Player GUI')
+#dlgMain.show() #показываем главное окно
+
+#app.exec_()  #запускаем приложение, здесь отслеживаются все события и вызываются соот. обработчики событий
+
+#sys.exit(app.exec_())
+
+class DlgMain(QDialog):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle('PlayMe') # добавляем виджет со свойствами
+
+#проверяем, импортирован ли модуль, не используется ли он в другой программе
+if __name__ = '__main___':     
+	app = QApplication(sys.argv) #создаем приложение
+	dlgMain = DlgMain() #создаем экземпляр класса DlgMain, который наследует от Qdialog
+	dlgMain.show()  #показываем графический пользовательский интерфейс
+	sys.exit(app.exec_()) # запускаем приложение
+
